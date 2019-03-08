203ffb77bce346680b09334f77fb095ee9f18c61
vincent <vincentwarmerdam@gmail.com>
2019-03-08 15:50:42 +0100

    holy cow. that was easier than expected

--- brent/query.py
+++ brent/query.py
@@ -14,7 +14,7 @@ from brent.common import normalise
 
 
 class Query:
-    def __init__(self, dag: DAG, given=None, do=None, counterfact=None):
+    def __init__(self, dag: DAG, given=None, do=None):
         """
         A `brent.Query` object describes a query that will be run
         on a `brent.DAG` object.
@@ -24,7 +24,6 @@ class Query:
         - **dag**: DAG object that contains all edges.
         - **given**: Dictionary of key-value pairs of given items.
         - **do**: Dictionary of key-value pairs of do operated items.
-        - **counterfact**: Dictionary of key-value pairs of counterfact operated items.
 
         Example:
 
@@ -45,11 +44,8 @@ class Query:
             given = dict()
         if not do:
             do = dict()
-        if not counterfact:
-            counterfact = dict()
         self.given_dict = given
         self.do_dict = do
-        self.counterfact = counterfact
 
     def inference_dag(self):
         """
@@ -85,7 +81,7 @@ class Query:
         - **kwargs**: key-value pairs of given items.
         """
         self._check_query_input(**kwargs)
-        return Query(dag=self.dag, given={**self.given_dict, **kwargs}, do=self.do_dict, counterfact=self.counterfact)
+        return Query(dag=self.dag, given={**self.given_dict, **kwargs}, do=self.do_dict)
 
     def do(self, **kwargs):
         """
@@ -96,18 +92,7 @@ class Query:
         - **kwargs**: key-value pairs of do items.
         """
         self._check_query_input(**kwargs)
-        return Query(dag=self.dag, given=self.given_dict, do={**self.do_dict, **kwargs}, counterfact=self.counterfact)
-
-    def counterfact(self, **kwargs):
-        """
-        Add items to the query that are observed but will be counterfacted.
-
-        ## Inputs
-
-        - **kwargs**: key-value pairs of observations to be counterfacted.
-        """
-        self._check_query_input(**kwargs)
-        return Query(dag=self.dag, given=self.given_dict, do=self.do_dict, counterfact={**self.counterfact, **kwargs})
+        return Query(dag=self.dag, given=self.given_dict, do={**self.do_dict, **kwargs})
 
     def plot(self):
         """
@@ -133,18 +118,6 @@ class Query:
             else:
                 d.edge(n1, n2)
         return d
-
-    def infer_counterfact(self):
-        """
-        If counterfact is in the query we will need to return.
-
-        The counterfacted variable is first assumed to be "given". We apply inference
-        on the entire graph as a first step. This gives us probabilities for all values.
-        This is the new graph definition.
-
-
-        """
-        pass
 
     def infer(self, give_table=False):
         """
@@ -203,3 +176,66 @@ class Query:
         idx = np.random.choice(table.index, p=table.prob, replace=True, size=n_samples)
         return table.loc[idx].reset_index(drop=True).drop(columns=['prob'])
 
+
+class SupposeQuery:
+    def __init__(self, dag, when=None, suppose_do=None, suppose_given=None):
+        self.dag = dag
+        self.orig_query = when
+        if not suppose_do:
+            suppose_do = dict()
+        if not suppose_given:
+            suppose_given = dict()
+        self.suppose_do_dict = suppose_do
+        self.suppose_given_dict = suppose_given
+
+    def _check_query_input(self, **kwargs):
+        for key, value in kwargs.items():
+            logging.debug(f"checking key {key}={value}")
+            if key not in self.dag.nodes:
+                raise ValueError(f"node '{key}' does not exist in original dag")
+            if value not in self.dag.df[key].values:
+                raise ValueError(f"value {value} does not occur for node {key}")
+            if key in {**self.suppose_do_dict, **self.suppose_given_dict}.keys():
+                raise ValueError(f"{key} is already used in this query")
+
+    def inference_dag(self):
+        """
+        This is a DAG created from the original but has been altered
+        to accomodate `do-calculus`.
+        """
+        infer_dag = DAG(self.dag.df.copy())
+        logging.debug(f"constructing copy of original DAG nodes: {infer_dag.nodes}")
+        for n1, n2 in self.dag.edges:
+            if n2 not in self.suppose_do_dict.keys():
+                infer_dag.add_edge(n1, n2)
+            else:
+                logging.debug(f"edge {n1} -> {n2} ignored because of do operator")
+        logging.debug(f"original DAG copied")
+        return infer_dag
+
+    def when(self, query):
+        if self.orig_query is not None:
+            raise ValueError("SupposeQuery already has a `.when` value assigned.")
+        return SupposeQuery(dag=self.dag, when=query, suppose_do=self.suppose_do_dict,
+                            suppose_given=self.suppose_given_dict)
+
+    def suppose_do(self, **kwargs):
+        self._check_query_input(**kwargs)
+        return SupposeQuery(dag=self.dag, when=self.orig_query, suppose_do={**self.suppose_do_dict, **kwargs},
+                            suppose_given=self.suppose_given_dict)
+
+    def suppose_given(self, **kwargs):
+        self._check_query_input(**kwargs)
+        return SupposeQuery(dag=self.dag, when=self.orig_query, suppose_do=self.suppose_do_dict,
+                            suppose_given={**self.suppose_given_dict, **kwargs})
+
+    def infer(self):
+        orig_query_table = self.orig_query.infer(give_table=True)
+        names_to_omit = list(self.orig_query.given_dict.keys()) + list(self.orig_query.do_dict.keys())
+        names_to_join = [n for n in q.dag.nodes if n not in names_to_omit]
+        new_query = Query(dag=self.dag, given=self.suppose_given_dict, do=self.suppose_do_dict).infer(give_table=True)
+        return (new_query
+                .merge(orig_query_table[names_to_join + ['prob']], on=names_to_join)
+                .assign(prob=lambda d: d.prob_y * d.prob_x)
+                .assign(prob=lambda d: d.prob / d.prob.sum())
+                .drop(columns=["prob_x", "prob_y"]))
---
+++ tests/test_suppose_query.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import pytest
+
+from brent import DAG
+from brent import Query, SupposeQuery
+
+
+@pytest.fixture
+def simple_dag():
+    df = pd.DataFrame({"a": [1, 1, 1, 1, 0, 0, 0, 0],
+                       "b": [0, 1, 0, 1, 1, 1, 1, 0],
+                       "c": [0, 0, 1, 0, 0, 1, 0, 1]})
+    return DAG(df).add_edge("a", "b").add_edge("a", "c").add_edge("c", "b")
+
+
+def test_value_error_with_when_twice(simple_dag):
+    q = Query(dag=simple_dag).given(b=1)
+    with pytest.raises(ValueError):
+        SupposeQuery(dag=simple_dag).when(q).when(q)
+
+
+def test_value_error_double_keys(simple_dag):
+    q = Query(dag=simple_dag).given(b=1)
+    with pytest.raises(ValueError):
+        SupposeQuery(dag=simple_dag).when(q).suppose_do(a=1).suppose_given(a=0)
+
+
+def test_value_error_wrong_value(simple_dag):
+    q = Query(dag=simple_dag).given(b=1)
+    with pytest.raises(ValueError):
+        SupposeQuery(dag=simple_dag).when(q).suppose_do(a=1000)
+
+
+def test_value_error_wrong_key(simple_dag):
+    q = Query(dag=simple_dag).given(b=1)
+    with pytest.raises(ValueError):
+        SupposeQuery(dag=simple_dag).when(q).suppose_do(foobar=1)
